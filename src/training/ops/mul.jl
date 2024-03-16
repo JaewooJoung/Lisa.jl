99a1a3d07163fd500a9af57012c7248b7fb926aa
Jaewoo Joung <89329303+JaewooJoung@users.noreply.github.com>
2024-03-16 21:49:41 +0100

    Update mul.jl

--- src/training/ops/mul.jl
+++ src/training/ops/mul.jl
@@ -1,5 +1,3 @@
-using CUDA
-
 struct Graph
     operations::Vector{Any}
 end
@@ -22,62 +20,33 @@ struct CuMulOp{T,N}
     output::CuArray{T,N}
 end
 
-if CUDA.functional()
-    function mul(input_a::CuArray{T,N}, input_b::CuArray{T,N}) where {T,N}
-        @assert size(input_a) == size(input_b)
-        output = CUDA.zeros(T, size(input_a))
-        push!(graph.operations, CuMulOp(input_a, input_b, output))
-        return output
-    end
+function mul(input_a::Tensor{T,N}, input_b::Tensor{T,N}) where {T,N}
+    @assert size(input_a.value) == size(input_b.value)
+    @assert input_a.graph == input_b.graph
+    graph = input_a.graph
 
-    function forward!(op::CuMulOp)
-        @. op.output = op.input_a * op.input_b
-        return nothing
-    end
+    output = Tensor(zeros(T, size(input_a.value)), zeros(T, size(input_a.value)), graph)
 
-    function backward!(op::CuMulOp)
-        input_a = op.input_a
-        ∂input_a = CUDA.zeros(eltype(input_a), size(input_a))
+    push!(graph.operations, MulOp(input_a, input_b, output))
+    return output
+end
 
-        input_b = op.input_b
-        ∂input_b = CUDA.zeros(eltype(input_b), size(input_b))
+function forward!(op::MulOp)
+    @. op.output.value = op.input_a.value * op.input_b.value
+    return nothing
+end
 
-        ∂output  = CUDA.zeros(eltype(op.output), size(op.output))
+function backward!(op::MulOp)
+    input_a = op.input_a.value
+    ∂input_a = op.input_a.grad
 
-        @. ∂input_a += ∂output * input_b
-        @. ∂input_b += ∂output * input_a
+    input_b = op.input_b.value
+    ∂input_b = op.input_b.grad
 
-        return nothing
-    end
-else
-    function mul(input_a::Tensor{T,N}, input_b::Tensor{T,N}) where {T,N}
-        @assert size(input_a.value) == size(input_b.value)
-        @assert input_a.graph == input_b.graph
-        graph = input_a.graph
+    ∂output  = op.output.grad
 
-        output = Tensor(zeros(T, size(input_a.value)), zeros(T, size(input_a.value)), graph)
+    @. ∂input_a += ∂output * input_b
+    @. ∂input_b += ∂output * input_a
 
-        push!(graph.operations, MulOp(input_a, input_b, output))
-        return output
-    end
-
-    function forward!(op::MulOp)
-        @. op.output.value = op.input_a.value * op.input_b.value
-        return nothing
-    end
-
-    function backward!(op::MulOp)
-        input_a = op.input_a.value
-        ∂input_a = op.input_a.grad
-
-        input_b = op.input_b.value
-        ∂input_b = op.input_b.grad
-
-        ∂output  = op.output.grad
-
-        @. ∂input_a += ∂output * input_b
-        @. ∂input_b += ∂output * input_a
-
-        return nothing
-    end
+    return nothing
 end
